3156583deef593c9930609d26c65f7a43a27950a
ADITYA SHARMA <sharmaaditya13064@gmail.com>
2023-05-08 23:02:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react';
 
-function App() {
+const App = ()=> {
+
+  const [value,setValue] = useState(null);
+  const [message,setMessage] = useState(null);
+  const [previousChats, setPreviousChats] = useState([]);
+  const [currentTitle,setCurrentTitle] = useState(null);
+
+
+  const createNewChat = () =>{
+    setMessage(null)
+    setValue("")
+    setCurrentTitle(null)
+  }
+
+  const handleClick = (uniqueTitle)=>{
+    setCurrentTitle(uniqueTitle)
+    setMessage(null)
+    setValue("")
+  }
+
+  const getMessages = async()=>{
+    const options = {
+      method:"POST",
+      body:JSON.stringify({
+        message:value
+      }),
+      headers:{
+        "Content-Type":"application/json"
+      }
+    }
+    try{
+      const response = await fetch('http://localhost:5000/completions',options);
+      const data = await response.json();
+     
+      setMessage(data.choices[0].message);
+    }catch(error){
+      console.log(error);
+    }
+  }
+   useEffect(()=>{
+    console.log(currentTitle,value,message);
+    if(!currentTitle && value && message)
+    {
+      setCurrentTitle(value);
+    }
+    if(currentTitle && value && message)
+    {
+      setPreviousChats(previousChats=>(
+        [...previousChats,
+          {
+            title:currentTitle,
+            role:"user",
+            content:value
+          },
+          {
+            title:currentTitle,
+            role:message.role,
+            content:message.content
+          }
+      ]
+      ))
+    }
+   },[message,currentTitle])
+
+   console.log(previousChats);
+  
+
+    const currentChat = previousChats.filter(previousChats => previousChats.title === currentTitle);
+    const uniqueTitle = Array.from(new Set(previousChats.map(previousChats => previousChats.title)));
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <section className="sidebar">
+        <button onClick={createNewChat} className="newChat-button">+ New Chat</button>
+        <ul className="history">
+          {uniqueTitle?.map((uniqueTitle,index)=><li key={index} onClick={()=>handleClick(uniqueTitle)} >{uniqueTitle}</li>)}
+        </ul>
+        <nav>
+          <p>Made by Aditya</p>
+        </nav>
+      </section>
+
+      <section className="main">
+        {!currentTitle && <h1>Eddy-GPT</h1>}
+        <ul className="feed">
+          {currentChat?.map((chatMessage,index) => <li key={index}>
+            <p className='role'>{chatMessage.role}</p>
+            <p>{chatMessage.content}</p>
+          </li>)}
+        </ul> 
+          
+        <div className="bottom-section">
+          <div className="input-container">
+            <input value={value} onChange={(e)=>setValue(e.target.value)} placeholder="Send a Message." />
+            <div id="submit" onClick={getMessages}>➢</div>
+          </div>
+          <p className="info">
+            Free Research Preview. ChatGPT may produce inaccurate information
+            about people, places, or facts. ChatGPT Mar 23 Version
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
